5ad60f4a711e55592af96d0d724ea4bd0804caa6
“claudio-code” <suporte2424@outlook.com>
2022-12-08 22:40:57 -0300

    added fist migrations

---
+++ infrastructure/src/main/resources/db/migration/V1__Initial.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS categories (
+    id CHAR(32) NOT NULL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    active BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at DATETIME(6) NOT NULL,
+    updated_at DATETIME(6) NOT NULL,
+    deleted_at DATETIME(6) NOT NULL
+);
