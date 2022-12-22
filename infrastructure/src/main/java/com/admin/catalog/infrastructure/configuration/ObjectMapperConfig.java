3ed1fbc56d1cbe6c89fbcc1f13c5b444086f9777
“claudio-code” <suporte2424@outlook.com>
2022-12-22 16:45:50 -0300

    added new annotations for tests

---
+++ infrastructure/src/main/java/com/admin/catalog/infrastructure/configuration/ObjectMapperConfig.java
@@ -0,0 +1,2 @@
+package com.admin.catalog.infrastructure.configuration;public class ObjectMapperConfig {
+}
---
+++ infrastructure/src/test/java/com/admin/catalog/infrastructure/IntegrationTest.java
@@ -0,0 +1,2 @@
+package com.admin.catalog.infrastructure;public @interface IntegrationTest {
+}
