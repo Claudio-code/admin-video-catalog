7183a92a73da76aac88dd86f7f58d0548c6262f0
“claudio-code” <suporte2424@outlook.com>
2023-01-21 03:55:36 -0300

    added swagger

--- infrastructure/src/main/java/com/admin/catalog/infrastructure/api/CategoryApi.java
+++ infrastructure/src/main/java/com/admin/catalog/infrastructure/api/CategoryApi.java
@@ -1,5 +1,34 @@
 package com.admin.catalog.infrastructure.api;
 
+import com.admin.catalog.domain.pagination.Pagination;
+import com.admin.catalog.infrastructure.category.models.CategoryListResponse;
+import com.admin.catalog.infrastructure.category.models.CreateCategoryRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RequestMapping(value = "categories")
+@Tag(name = "Categories")
 public interface CategoryApi {
+
+    @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    @Operation(summary = "Create a new category")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Created with success"),
+        @ApiResponse(responseCode = "422", description = "A validation error was thrown"),
+        @ApiResponse(responseCode = "500", description = "An internal server error was thrown"),
+    })
+    ResponseEntity<?> createCategory(@RequestBody CreateCategoryRequest input);
+
+    Pagination<CategoryListResponse> listCategories(
+
+    );
 
 }
