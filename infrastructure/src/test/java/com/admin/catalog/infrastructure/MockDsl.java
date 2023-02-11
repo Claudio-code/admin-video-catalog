fb3c941b6ed4ee4acb0a791c22191ea3158259ae
“claudio-code” <suporte2424@outlook.com>
2023-02-11 17:44:42 -0300

    added e2e config to tests

--- infrastructure/src/test/java/com/admin/catalog/infrastructure/MockDsl.java
+++ infrastructure/src/test/java/com/admin/catalog/infrastructure/MockDsl.java
@@ -12,8 +12,10 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Function;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -25,7 +27,11 @@ public interface MockDsl {
         return this.delete("/categories/", anId);
     }
 
-    default CategoryID givenACategory(final String aName, final String aDescription, final boolean isActive) throws Exception {
+    default CategoryID givenACategory(
+        final String aName,
+        final String aDescription,
+        final boolean isActive
+    ) throws Exception {
         final var aRequestBody = new CreateCategoryRequest(aName, aDescription, isActive);
         final var actualId = this.given("/categories", aRequestBody);
         return CategoryID.from(actualId);
@@ -39,7 +45,13 @@ public interface MockDsl {
         return listCategories(page, perPage, search, "", "");
     }
 
-    default ResultActions listCategories(final int page, final int perPage, final String search, final String sort, final String direction) throws Exception {
+    default ResultActions listCategories(
+        final int page,
+        final int perPage,
+        final String search,
+        final String sort,
+        final String direction
+    ) throws Exception {
         return this.list("/categories", page, perPage, search, sort, direction);
     }
 
@@ -59,79 +71,69 @@ public interface MockDsl {
 
     private String given(final String url, final Object body) throws Exception {
         final var aRequest = post(url)
-            .with(ApiTest.ADMIN_JWT)
-            .contentType(MediaType.APPLICATION_JSON)
+            .contentType(APPLICATION_JSON)
             .content(Json.writeValueAsString(body));
-
-        final var actualId = this.mvc().perform(aRequest)
-            .andExpect(status().isCreated())
-            .andReturn()
-            .getResponse().getHeader("Location")
+        return Objects.requireNonNull(this.mvc().perform(aRequest)
+                .andExpect(status().isCreated())
+                .andReturn()
+                .getResponse().getHeader("Location"))
             .replace("%s/".formatted(url), "");
-
-        return actualId;
     }
 
     private ResultActions givenResult(final String url, final Object body) throws Exception {
         final var aRequest = post(url)
-            .with(ApiTest.ADMIN_JWT)
-            .contentType(MediaType.APPLICATION_JSON)
+            .contentType(APPLICATION_JSON)
             .content(Json.writeValueAsString(body));
-
         return this.mvc().perform(aRequest);
     }
 
-    private ResultActions list(final String url, final int page, final int perPage, final String search, final String sort, final String direction) throws Exception {
+    private ResultActions list(
+        final String url,
+        final int page,
+        final int perPage,
+        final String search,
+        final String sort,
+        final String direction
+    ) throws Exception {
         final var aRequest = get(url)
-            .with(ApiTest.ADMIN_JWT)
             .queryParam("page", String.valueOf(page))
             .queryParam("perPage", String.valueOf(perPage))
             .queryParam("search", search)
             .queryParam("sort", sort)
             .queryParam("dir", direction)
-            .accept(MediaType.APPLICATION_JSON)
-            .contentType(MediaType.APPLICATION_JSON);
-
+            .accept(APPLICATION_JSON)
+            .contentType(APPLICATION_JSON);
         return this.mvc().perform(aRequest);
     }
 
     private <T> T retrieve(final String url, final Identifier anId, final Class<T> clazz) throws Exception {
         final var aRequest = get(url + anId.getValue())
-            .with(ApiTest.ADMIN_JWT)
-            .accept(MediaType.APPLICATION_JSON_UTF8)
-            .contentType(MediaType.APPLICATION_JSON_UTF8);
-
+            .accept(APPLICATION_JSON)
+            .contentType(APPLICATION_JSON);
         final var json = this.mvc().perform(aRequest)
             .andExpect(status().isOk())
             .andReturn()
             .getResponse().getContentAsString();
-
         return Json.readValue(json, clazz);
     }
 
     private ResultActions retrieveResult(final String url, final Identifier anId) throws Exception {
         final var aRequest = get(url + anId.getValue())
-            .with(ApiTest.ADMIN_JWT)
             .accept(MediaType.APPLICATION_JSON_UTF8)
             .contentType(MediaType.APPLICATION_JSON_UTF8);
-
         return this.mvc().perform(aRequest);
     }
 
     private ResultActions delete(final String url, final Identifier anId) throws Exception {
         final var aRequest = MockMvcRequestBuilders.delete(url + anId.getValue())
-            .with(ApiTest.ADMIN_JWT)
-            .contentType(MediaType.APPLICATION_JSON);
-
+            .contentType(APPLICATION_JSON);
         return this.mvc().perform(aRequest);
     }
 
     private ResultActions update(final String url, final Identifier anId, final Object aRequestBody) throws Exception {
         final var aRequest = MockMvcRequestBuilders.put(url + anId.getValue())
-            .with(ApiTest.ADMIN_JWT)
-            .contentType(MediaType.APPLICATION_JSON)
+            .contentType(APPLICATION_JSON)
             .content(Json.writeValueAsString(aRequestBody));
-
         return this.mvc().perform(aRequest);
     }
 
--- infrastructure/src/test/java/com/admin/catalog/infrastructure/category/e2e/CategoryE2ETest.java
+++ infrastructure/src/test/java/com/admin/catalog/infrastructure/category/e2e/CategoryE2ETest.java
@@ -1,4 +1,43 @@
 package com.admin.catalog.infrastructure.category.e2e;
 
-public class CategoryE2ETest {
+import com.admin.catalog.infrastructure.E2ETest;
+import com.admin.catalog.infrastructure.MockDsl;
+import com.admin.catalog.infrastructure.category.persistence.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@E2ETest
+@Testcontainers
+public class CategoryE2ETest implements MockDsl {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Container
+    private static final MySQLContainer MYSQL_CONTAINER = new MySQLContainer("mysql:latest")
+        .withPassword("123456")
+        .withUsername("root")
+        .withDatabaseName("catalog");
+
+    @Override
+    public MockMvc mvc() {
+        return mvc;
+    }
+
+    @DynamicPropertySource
+    public static void setDatasourceProperties(final DynamicPropertyRegistry registry) {
+        registry.add("mysql.port", () -> MYSQL_CONTAINER.getMappedPort(3306));
+    }
+
+
+
+
 }
