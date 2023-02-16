c6bef1d54e3b29e9e460fcaf364373fae3a4863a
“claudio-code” <suporte2424@outlook.com>
2023-02-15 20:54:02 -0500

    created first e2e test

--- infrastructure/src/test/java/com/admin/catalog/infrastructure/category/e2e/CategoryE2ETest.java
+++ infrastructure/src/test/java/com/admin/catalog/infrastructure/category/e2e/CategoryE2ETest.java
@@ -1,8 +1,15 @@
 package com.admin.catalog.infrastructure.category.e2e;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import com.admin.catalog.infrastructure.E2ETest;
 import com.admin.catalog.infrastructure.MockDsl;
 import com.admin.catalog.infrastructure.category.persistence.CategoryRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -37,7 +44,25 @@ public class CategoryE2ETest implements MockDsl {
         registry.add("mysql.port", () -> MYSQL_CONTAINER.getMappedPort(3306));
     }
 
+    @Test
+    public void asACatalogAdminIShouldBeAbleToCreateANewCategoryWithValidValues() throws Exception {
+        assertTrue(MYSQL_CONTAINER.isCreated());
+        assertEquals(0, categoryRepository.count());
 
+        final var expectedName = "Movies";
+        final var expectedDescription = "The new best of all";
+        final var expectedIsActive = true;
 
+        final var actualId = givenACategory(expectedName, expectedDescription, expectedIsActive);
+
+        final var actualCategory = categoryRepository.findById(actualId.getValue()).get();
+
+        assertEquals(expectedIsActive, actualCategory.isActive());
+        assertEquals(expectedDescription, actualCategory.getDescription());
+        assertEquals(expectedName, actualCategory.getName());
+        assertNotNull(actualCategory.getCreatedAt());
+        assertNotNull(actualCategory.getUpdatedAt());
+        assertNull(actualCategory.getDeletedAt());
+    }
 
 }
