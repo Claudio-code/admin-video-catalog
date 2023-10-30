aa9a745fba3eeef6c0f1a31c3edecf0100886e70
“claudio-code” <suporte2424@outlook.com>
2023-10-30 08:45:43 -0300

    added test of list genres

--- infrastructure/src/test/java/com/admin/catalog/infrastructure/genre/e2e/GenresE2ETest.java
+++ infrastructure/src/test/java/com/admin/catalog/infrastructure/genre/e2e/GenresE2ETest.java
@@ -1,8 +1,10 @@
 package com.admin.catalog.infrastructure.genre.e2e;
 
+import com.admin.catalog.domain.category.CategoryID;
 import com.admin.catalog.infrastructure.E2ETest;
 import com.admin.catalog.infrastructure.MockDsl;
 import com.admin.catalog.infrastructure.genre.persistence.GenreRepository;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -10,6 +12,14 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @E2ETest
 @Testcontainers
@@ -33,6 +43,85 @@ public class GenresE2ETest implements MockDsl {
     @DynamicPropertySource
     public static void setDatasourceProperties(final DynamicPropertyRegistry registry) {
         registry.add("mysql.port", () -> MYSQL_CONTAINER.getMappedPort(3306));
+    }
+
+    @Test
+    void asACatalogAdminIShouldBeAbleToCreateAnewGenreWithValidValues() throws Exception {
+        assertTrue(MYSQL_CONTAINER.isRunning());
+        assertEquals(0, genreRepository.count());
+
+        final var expectedName = "Horror";
+        final var expectedIsActive = true;
+        final List<CategoryID> expectedCategories = List.of();
+
+        final var actualId = givenAGenre(expectedName, expectedIsActive, expectedCategories);
+        final var actualGenre = genreRepository.findById(actualId.getValue()).get();
+
+        assertEquals(expectedName, actualGenre.getName());
+        assertEquals(expectedIsActive, actualGenre.isActive());
+        assertTrue(
+            expectedCategories.size() == actualGenre.getCategories().size()
+                && expectedCategories.containsAll(actualGenre.getCategories())
+        );
+        assertNotNull(actualGenre.getCreatedAt());
+        assertNotNull(actualGenre.getUpdatedAt());
+        assertNull(actualGenre.getDeletedAt());
+    }
+
+    @Test
+    void asACatalogAdminIShouldBeAbleToCreateANewGenreWithCategories() throws Exception {
+        assertTrue(MYSQL_CONTAINER.isRunning());
+        assertEquals(0, genreRepository.count());
+
+        final var expectedName = "Horror";
+        final var expectedIsActive = true;
+        final var expectedCategories = List.of(givenACategory("Hard work", null, true));
+        final var actualId = givenAGenre(expectedName, expectedIsActive, expectedCategories);
+        final var actualGenre = genreRepository.findById(actualId.getValue()).get();
+
+        assertEquals(expectedName, actualGenre.getName());
+        assertEquals(expectedIsActive, actualGenre.isActive());
+        assertTrue(
+            expectedCategories.size() == actualGenre.getCategories().size()
+                && expectedCategories.containsAll(actualGenre.getCategories())
+        );
+        assertNotNull(actualGenre.getCreatedAt());
+        assertNotNull(actualGenre.getUpdatedAt());
+        assertNull(actualGenre.getDeletedAt());
+    }
+
+    @Test
+    void asACatalogAdminIShouldBeAbleToNavigateThruAllGenres() throws Exception {
+        assertTrue(MYSQL_CONTAINER.isRunning());
+        assertEquals(0, genreRepository.count());
+
+        givenAGenre("Action", true, List.of());
+        givenAGenre("Horror", true, List.of());
+        givenAGenre("Sport", true, List.of());
+
+        listGenres(0, 1)
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.currentPage", equalTo(0)))
+            .andExpect(jsonPath("$.perPage", equalTo(1)))
+            .andExpect(jsonPath("$.total", equalTo(3)))
+            .andExpect(jsonPath("$.items", hasSize(1)))
+            .andExpect(jsonPath("$.items[0].name", equalTo("Action")));
+
+        listGenres(1, 1)
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.currentPage", equalTo(1)))
+            .andExpect(jsonPath("$.perPage", equalTo(1)))
+            .andExpect(jsonPath("$.total", equalTo(3)))
+            .andExpect(jsonPath("$.items", hasSize(1)))
+            .andExpect(jsonPath("$.items[0].name", equalTo("Horror")));
+
+        listGenres(2, 1)
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.currentPage", equalTo(2)))
+            .andExpect(jsonPath("$.perPage", equalTo(1)))
+            .andExpect(jsonPath("$.total", equalTo(3)))
+            .andExpect(jsonPath("$.items", hasSize(1)))
+            .andExpect(jsonPath("$.items[0].name", equalTo("Sport")));
     }
 
 }
